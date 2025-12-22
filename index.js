a175405ae5cbad89b6e3c02ae0d24711c08c472d
ReObscura <TheReObscura@gmail.com>
2025-12-22 12:19:28 +0300

    Дополнено данным из вводного задания

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 require('dotenv').config(); 
 console.log("Текущий режим работы:", process.env.MODE);
+console.log("Имя:", process.env.NAME);
+console.log("Фамилия:", process.env.SURNAME);
+console.log("Группа:", process.env.GROUP);
+console.log("Номер по списку:", process.env.NUMBER);
+console.log("Режим работы:", process.env.MODE);
